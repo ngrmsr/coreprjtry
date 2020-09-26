0e9c596e3bce85edf4eb4a75b307247b195cb842
negarmesri <negarmesry@gmail.com>
2020-09-26 17:31:51 +0330

    use concurency ckeck an timestamp in loanmodel

--- TryCore/Model/LoanModel.cs
+++ TryCore/Model/LoanModel.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 using Try_Core_Entities.Model;
 
 namespace Try_Core_Entities
 {
+    [ConcurrencyCheck]
    public class LoanModel
     {
         public int LoanModelId { get; set; }
